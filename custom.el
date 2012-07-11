54e01aa7cef0bd11de475e0b26da31c4858e1579
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2012-07-11 20:01:40 +0300

    Disabled CUA mode keys

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cua-enable-cua-keys nil)
  '(cua-enable-cursor-indications t)
  '(cua-mode t nil (cua-base))
  '(cua-normal-cursor-color "snow")
