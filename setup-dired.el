44fd07dae929a788cd848fd87a94563fa6aaad05
Giorgos Pontikakis <giorgos.pontikakis@gmail.com>
2013-03-15 00:03:21 +0200

    Better keys for dired mode

--- setup-dired.el
+++ setup-dired.el
@@ -1,6 +1,4 @@
 (require 'dired)
-(require 'dired-x)
-
 
 ;; Reload dired after creating a directory or creating af file
 (defadvice dired-create-directory (after revert-buffer-after-create activate)
@@ -25,29 +23,25 @@
   (end-of-buffer)
   (dired-previous-line 1))
 
-;;; Bind the keys
-;;; Extend key bindings to mimic wgrep
+;;; Bind some keys
 (define-key dired-mode-map (kbd "C-<up>") 'dired-jump-to-top)
 (define-key dired-mode-map (kbd "C-<down>") 'dired-jump-to-bottom)
 (define-key dired-mode-map (kbd "M-<up>") 'dired-up-directory)
+(define-key dired-mode-map (kbd "M-o") nil)
+
+;;; Extend key bindings to mimic wgrep and discard remapping of
+;;; read-only-mode command to dired-toggle-read-only (see dired.el)
 (define-key dired-mode-map (kbd "C-c C-w") 'wdired-change-to-wdired-mode)
+(define-key dired-mode-map [remap read-only-mode] nil)
 
 (eval-after-load "wdired"
   '(progn
      (define-key wdired-mode-map (kbd "C-<up>") 'dired-jump-to-top)
      (define-key wdired-mode-map (kbd "C-<down>") 'dired-jump-to-bottom)
+     ;; mimic wgrep keys
      (define-key wdired-mode-map (kbd "C-c C-k") 'wdired-abort-changes)
-     (define-key wdired-mode-map (kbd "C-x C-s") 'wdired-finish-changes)))
-
-(defun dired-key-bindings ()
-  (interactive)
-  (define-key dired-mode-map (kbd "M-o") nil)
-  (define-key dired-mode-map [remap toggle-read-only] nil)
-  (define-key dired-mode-map (kbd "C-x M-q") 'dired-toggle-read-only))
-
-(eval-after-load "dired"
-  '(dired-key-bindings))
-
-
+     (define-key wdired-mode-map (kbd "C-x C-s") 'wdired-finish-edit)
+     (define-key wdired-mode-map (kbd "C-c ESC") nil)
+     (define-key wdired-mode-map (kbd "C-c C-c") nil)))
 
 (provide 'setup-dired)
